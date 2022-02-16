9fb6392e5c4c2b7416a5e29501da02c079f314cc
Kaddrianne <99816272+Kaddrianne@users.noreply.github.com>
2022-02-16 22:33:00 +0800

    Add files via upload

---
+++ mod01-mp01.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            mod01-mp01 
+        </title>
+</head>
+<body>
+    <h4>PERSONAL INFORMATION</h4>
+    <p> Name: Kyle Addrianne San Antonio</p>
+    <p> Birthday: March 02, 2001</p>
+    <p> Gender: Male</p>
+    <p> Home Address: San Francisco, Magalang, Pampanga</p>
+    <br>
+    <p>
+        <h4> FAMILY BACKGROUND</h4>
+        <p> Father's Name: Jeffrey San Antonio</p>
+        <p> Mother's Name: Arlene Panlaqui</p>
+        <p> Siblings: N/A </p>
+        <br>
+        <p>
+            <h4> EDUCATIONAL BACKGROUND</h4>
+            <p> Primary School: Hillcrest Heights Institute</p>
+            <p> Date Graduated: -</p>
+            <p> Junior Highschool: Hillcrest Heights Institute</p>
+            <p> Date Graduated: 2017</p>
+            <p>Senior High School: Hillcrest Heights Institute</p>
+            <p> Date Graduated: 2019</p>
+            <p> Tertiary School: Angeles University Foundation</p>
+            <p> Course: BS Computer Engineering</p>
+            <br>
+            <p>
+                <h4>FAVORITE FOODS</p>
+                <p> 1. Adobo</p>
+                <p> 2. Chicken Karaage</p>
+                <p> 3. Egg</p>
+                <p> 4. French Fries </p>
+                <p> 5. Noodles </p>
+                <br>
+                <p>
+                    <h4>FAVORITE MOVIE</h4>
+                    <p> 1. SPIDERMAN</p>
+                    <p> 2. STARWARS</p>
+                    <p> 3. GHIBLI MOVIES</p>
+                    <p> 4. ANIME MOVIES</p>
+                    <p> 5. 100 TULA PARA KAY STELLA</p>
+                    <br>
+                    <p>
+                        <h4> FAVORITE SONGS</h4>
+                        <p> 1. Every summertime</p>
+                        <p> 2. Araw araw Ben&Ben </p>
+                        <p> 3. Yellow</p>
+                        <p> 4. Paraluman</p>
+                        <p> 5. Rainbow</p>
+</body>
+<html>
