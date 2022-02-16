4782625d8f2ef0369bdb0ca91b1ae1e789f3bdca
Kaddrianne <99816272+Kaddrianne@users.noreply.github.com>
2022-02-16 23:32:24 +0800

    Update mod01-mp01.php

--- mod01-mp01.php
+++ mod01-mp01.php
@@ -11,6 +11,7 @@
     <p> Birthday: March 02, 2001</p>
     <p> Gender: Male</p>
     <p> Home Address: San Francisco, Magalang, Pampanga</p>
+    <p> Email Address: sanantonio.kyleaddrianne@auf.edu.ph</p>
     <br>
     <p>
         <h4> FAMILY BACKGROUND</h4>
@@ -30,7 +31,7 @@
             <p> Course: BS Computer Engineering</p>
             <br>
             <p>
-                <h4>FAVORITE FOODS</h4>
+                <h4>FAVORITE FOODS</p>
                 <p> 1. Adobo</p>
                 <p> 2. Chicken Karaage</p>
                 <p> 3. Egg</p>
@@ -54,3 +55,59 @@
                         <p> 5. Rainbow</p>
 </body>
 <html>
+    <head>
+        <title>
+            mod01-mp01
+        </title>
+</head>
+<body>
+    <?php
+    echo "<b>PERSONAL INFORMATION</b><br>";
+    echo "Name: Kyle Addrianne San Antonio <br>";
+    echo "Birthday: March 02, 2001 <br>";
+    echo "Gender: Male <br>";
+    echo "Home Address: San Francisco, Magalang, Pampanga <br>";
+
+    echo "<b>FAMILY BACKGROUND </b><br>";
+    echo "Father's Name: Jeffrey San Antonio <br>";
+    echo "Mother's Name: Arlene Panlaqui <br>";
+    echo "Siblings: N/A  <br>";
+
+    echo "<b>EDUCATIONAL BACKGROUND </b><br>";
+    echo "Primary School: Hillcrest Heights Institute <br>";
+    echo "Date Graduated: - <br>";
+    echo "Junior Highschool: Hillcrest Heights Institute <br>";
+    echo "Date Graduated: 2017 <br>";
+    echo "Senior High School: Hillcrest Heights Institute <br>";
+    echo "Date Graduated: 2019 <br>";
+    echo "Tertiary School: Angeles University Foundation <br>";
+    echo "Course: BS Computer Engineering <br>";
+
+    echo "<b>FAVORITE FOODS</b> <br>";
+    echo "1. Adobo <br>";
+    echo "2. Chicken Karaage <br>";
+    echo "3. Egg <br>";
+    echo "4. French Fries  <br>";
+    echo "5. Noodles  <br>";
+
+    echo "<b>FAVORITE MOVIE </b><br>";
+    echo "1. SPIDERMAN <br>";
+    echo "2. STARWARS <br>";
+    echo "3. GHIBLI MOVIES <br>";
+    echo "4. ANIME MOVIES <br>";
+    echo "5. 100 TULA PARA KAY STELLA <br>";
+
+    echo "<b>FAVORITE SONGS</b> <br>";
+    echo "1. Every summertime <br>";
+    echo "2. Araw araw Ben&Ben  <br>";
+    echo "3. Yellow <br>";
+    echo "4. Paraluman <br>";
+    echo "5. Rainbow <br>";
+?>
+</body>
+<html>
+
+    
+    
+    
+</body>
